2da7979eed2516a239cbca76d75d5f274e763453
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-07 18:44:48 +0000

    procedures returning year month day

---
+++ months.py
@@ -0,0 +1,17 @@
+###
+### Define a simple nextDay procedure, that assumes
+### every month has 30 days.
+###
+### For example:
+###    nextDay(2013, 1, 30) => (2013, 2, 1)
+###    nextDay(2012, 12, 30) => (2013, 1, 1)  (even though December really has 31 days)
+###
+
+def nextDay(year, month, day):
+    if day < 30:
+        return year, month, day + 1
+    else:
+        if month < 12:
+            return year, month + 1, 1
+        else:
+            return year + 1, 1, 1
