7115380f001a4706da2f2ca2607d2e2f5972d134
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-28 18:18:26 +0000

    objects

---
+++ object.py
@@ -0,0 +1,13 @@
+class Program():
+    
+    def __init__(self, *args, **kwargs):
+        
+        self.lang = input("What language? : ")
+        self.version = float(input("Version?:"))
+        self.skill = input("What skill level?")
+        
+        p1 = Program()
+        
+        
+    
+        
